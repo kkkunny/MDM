a5f9724f35c9a3361ef6f2aaf5c7f58cc4c477e3
kkkunny <1401289859@qq.com>
2026-03-09 22:19:09 +0800

    fix: 使用迅雷判断是否是单文件任务

--- service/task/task.go
+++ service/task/task.go
@@ -160,17 +160,29 @@ func Completed(ctx context.Context, tasks ...*dto.XLTask) error {
 			return err
 		}
 		if exist {
+			resources, err := stlerr.ErrorWith(xl.Client.ListResource(ctx, task.URL))
+			if err != nil {
+				return err
+			}
+			singleFile := !stlslices.First(resources).IsDir()
+			filename := stlslices.First(resources).GetName()
 			fromPath := task.SavePath()
-			toPath := filepath.Join(config.DownloadCompleteDir, tmi.Name)
-			singleFile := len(tmi.Files) == 1
 			if singleFile {
 				// 单文件时迅雷会自动创建一个文件夹
-				fromPath = filepath.Join(fromPath, tmi.Name)
+				fromPath = filepath.Join(fromPath, filename)
 			}
+			toPath := filepath.Join(config.DownloadCompleteDir, filename)
 			err = stlerr.ErrorWrap(os.Rename(fromPath, toPath))
 			if err != nil {
 				_ = config.Logger.Warnf("tmi=%s, fromPath=%s, toPath=%s, xltask=%s", util.ToJson[string](tmi), fromPath, toPath, util.ToJson[string](task.TaskInfo))
 				return err
+			}
+			// 单文件时迅雷会自动创建一个文件夹，需要删除
+			if singleFile {
+				err = stlerr.ErrorWrap(os.RemoveAll(filepath.Dir(fromPath)))
+				if err != nil {
+					return err
+				}
 			}
 		}
 
