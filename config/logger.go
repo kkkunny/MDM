2ce8bfab4bf9b4861028a7264dd12df7bf2c000f
kkkunny <1401289859@qq.com>
2026-03-28 13:20:31 +0800

    feat: 日志等级自定义

--- config/logger.go
+++ config/logger.go
@@ -1,7 +1,34 @@
 package config
 
 import (
+	"os"
+
 	stllog "github.com/kkkunny/stl/log"
+	stlval "github.com/kkkunny/stl/value"
 )
 
-var Logger = stllog.Default(!Release)
+var Logger *stllog.Logger
+
+func init() {
+	var level stllog.Level
+	switch os.Getenv("LOG_LEVEL") {
+	case "DEBUG":
+		level = stllog.LevelDebug
+	case "TRACE":
+		level = stllog.LevelTrace
+	case "INFO":
+		level = stllog.LevelInfo
+	case "WARN":
+		level = stllog.LevelWarn
+	case "KEYWORD":
+		level = stllog.LevelKeyword
+	case "ERROR":
+		level = stllog.LevelError
+	case "PANIC":
+		level = stllog.LevelPanic
+	default:
+		level = stlval.Ternary(Release, stllog.LevelInfo, stllog.LevelDebug)
+	}
+	Logger = stllog.New(os.Stdout, level)
+	Logger.SetDefaultConfig(Logger.WithDisplayLevel().WithDisplayTime().WithDisplayPosition().WithDisplayColor().WithDisplayGroup())
+}
